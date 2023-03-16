f532c9ea724c102e51194831963d97f4f8227e4b
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-03-17 00:09:11 +0900

    chore : 스프링 프로젝트 세팅

---
+++ src/main/java/com/sosim/server/SosimApplication.java
@@ -0,0 +1,13 @@
+package com.sosim.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosimApplication.class, args);
+	}
+
+}
