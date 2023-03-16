ddc32bf622962ff9f159fe9bf923b8c49bbafdfb
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-03-17 02:49:05 +0900

    feat : Redis 추가

--- src/main/java/com/sosim/server/jwt/RefreshToken.java
+++ src/main/java/com/sosim/server/jwt/RefreshToken.java
@@ -3,12 +3,16 @@ package com.sosim.server.jwt;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
 @Getter
+@RedisHash
 @Builder(access = AccessLevel.PRIVATE)
 public class RefreshToken {
 
-    private Long id;
+    @Id
+    private String id;
 
     private String refreshToken;
 }
