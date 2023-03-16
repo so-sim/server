91a16977425d51386060c3320419423522c706ef
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-03-17 02:18:04 +0900

    feat : Sever Jwt 생성 주체 정의

---
+++ src/main/java/com/sosim/server/jwt/JwtFactory.java
@@ -0,0 +1,59 @@
+package com.sosim.server.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtFactory {
+
+    @Value("${jwt.access.key}")
+    private String accessKey;
+
+    @Value("${jwt.refresh.key}")
+    private String refreshKey;
+
+    @Value("${jwt.access.expiration}")
+    private Long accessExpiration;
+
+    @Value("${jwt.refresh.expiration}")
+    private Long refreshExpiration;
+
+    private String encodeAccessKey;
+    private String encodeRefreshKey;
+
+    @PostConstruct
+    protected void init(){
+        encodeAccessKey = Base64.getEncoder().encodeToString(accessKey.getBytes());
+        encodeRefreshKey = Base64.getEncoder().encodeToString(refreshKey.getBytes());
+    }
+
+    public String createAccessToken(User user){
+
+        Claims claims = Jwts.claims().setSubject(user.getId().toString());
+
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + accessExpiration))
+                .signWith(SignatureAlgorithm.HS256, encodeAccessKey)
+                .compact();
+    }
+
+    public String createRefreshToken(){
+        Date now = new Date();
+        return Jwts.builder()
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + refreshExpiration))
+                .signWith(SignatureAlgorithm.HS256, encodeRefreshKey)
+                .compact();
+    }
+}
