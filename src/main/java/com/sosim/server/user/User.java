61e489c770938529af2b514c978c7445acee5f93
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-03-17 17:58:30 +0900

    feat : User 필드 추가, UserRepository 작성

--- src/main/java/com/sosim/server/user/User.java
+++ src/main/java/com/sosim/server/user/User.java
@@ -1,14 +1,34 @@
 package com.sosim.server.user;
 
+import com.sosim.server.oauth.Social;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Getter
+@Entity
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String email;
+
+    private Social social;
+
+    private Long socialId;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private User(String email, Social social, Long socialId) {
+        this.email = email;
+        this.social = social;
+        this.socialId = socialId;
+    }
 }
---
+++ src/main/java/com/sosim/server/user/UserRepository.java
@@ -0,0 +1,8 @@
+package com.sosim.server.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
