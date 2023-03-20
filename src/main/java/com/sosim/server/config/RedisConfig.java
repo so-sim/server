124ba4aee0e16f105d163bd04cd727418bd8f0fe
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-03-21 02:25:26 +0900

    fix : redis host, port 기본값 지정

--- src/main/java/com/sosim/server/config/RedisConfig.java
+++ src/main/java/com/sosim/server/config/RedisConfig.java
@@ -13,10 +13,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableRedisRepositories
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
+    @Value("${spring.redis.host:localhost}")
     private String host;
 
-    @Value("${spring.redis.port}")
+    @Value("${spring.redis.port:6379}")
     private int port;
 
     @Bean
