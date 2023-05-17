ac8cb4b4f4db810638ba82da9f2916d97e7a10c5
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-05-17 16:37:43 +0900

    feat : 커스텀 예외 클래스 추가

--- src/main/java/com/sosim/server/common/response/ResponseCode.java
+++ src/main/java/com/sosim/server/common/response/ResponseCode.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
-public enum ResponseType {
+public enum ResponseCode {
 
     SUCCESS_LOGIN(successCode(), HttpStatus.OK, "로그인이 성공적으로 완료되었습니다.");
 
