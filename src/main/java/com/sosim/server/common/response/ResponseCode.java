afcb1636b0808451c13e9ac8d3386d61fb62f5d1
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-05-30 16:12:48 +0900

    feat : Cookie 안의 RefreshToken 가져오기 기능 추가

--- src/main/java/com/sosim/server/common/response/ResponseCode.java
+++ src/main/java/com/sosim/server/common/response/ResponseCode.java
@@ -11,6 +11,8 @@ public enum ResponseCode {
     SUCCESS_LOGIN(successCode(), HttpStatus.OK, "로그인이 성공적으로 완료되었습니다."),
     SUCCESS_SIGNUP(successCode(), HttpStatus.OK, "회원가입이 성공적으로 완료되었습니다."),
 
+    NOT_EXIST_TOKEN_COOKIE(1200, HttpStatus.BAD_REQUEST, "리프레시 토큰이 존재하지 않습니다."),
+
     NOT_FOUND_USER(1100, HttpStatus.BAD_REQUEST, "존재하지 않는 회원 정보입니다."),
     USER_ALREADY_EXIST(1101, HttpStatus.BAD_REQUEST, "이미 존재하는 회원 정보입니다.");
 
