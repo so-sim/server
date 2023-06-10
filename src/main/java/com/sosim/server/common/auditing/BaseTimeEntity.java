9feae33d845c16176e61cdaa76126325cc11e7b6
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-06-10 18:42:13 +0900

    fix : 오류 수정

--- src/main/java/com/sosim/server/common/auditing/BaseTimeEntity.java
+++ src/main/java/com/sosim/server/common/auditing/BaseTimeEntity.java
@@ -3,13 +3,14 @@ package com.sosim.server.common.auditing;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AutoCloseable.class)
+@EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
     @Column(name = "CREATE_DATE", updatable = false)
     @CreatedDate
