65491dc4f9c3515c8e4d8ef645c0eba5432d3cf6
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-06-10 16:15:07 +0900

    feat : Group 생성 시 Participant 생성 로직 추가

--- src/main/java/com/sosim/server/group/GroupService.java
+++ src/main/java/com/sosim/server/group/GroupService.java
@@ -2,6 +2,9 @@ package com.sosim.server.group;
 
 import com.sosim.server.group.dto.request.CreateGroupRequest;
 import com.sosim.server.group.dto.response.CreateGroupResponse;
+import com.sosim.server.participant.ParticipantService;
+import com.sosim.server.user.User;
+import com.sosim.server.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +13,13 @@ import org.springframework.stereotype.Service;
 public class GroupService {
 
     private final GroupRepository groupRepository;
+    private final UserService userService;
+    private final ParticipantService participantService;
 
     public CreateGroupResponse createGroup(Long userId, CreateGroupRequest createGroupRequest) {
+        User userEntity = userService.getUser(userId);
         Group groupEntity = saveGroupEntity(Group.create(userId, createGroupRequest));
+        participantService.creteParticipant(userEntity, groupEntity, createGroupRequest.getNickname());
 
         return CreateGroupResponse.create(groupEntity);
     }
