75e4f49f6e842746690ea3a6f3ffaf930083b7e8
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-06-10 18:03:46 +0900

    feat : Group 조회 Response 상태 값 추가

--- src/main/java/com/sosim/server/common/response/ResponseCode.java
+++ src/main/java/com/sosim/server/common/response/ResponseCode.java
@@ -14,6 +14,7 @@ public enum ResponseCode {
     SUCCESS_LOGOUT(successCode(), HttpStatus.OK, "로그아웃이 성공적으로 완료되었습니다."),
 
     CREATE_GROUP(successCode(), HttpStatus.CREATED, "모임이 성공적으로 생성되었습니다."),
+    GET_GROUP(successCode(), HttpStatus.OK, "모임이 성공적으로 조회되었습니다."),
 
     BINDING_ERROR(2000, HttpStatus.BAD_REQUEST, "입력값 중 검증에 실패한 값이 있습니다"),
 
@@ -22,6 +23,7 @@ public enum ResponseCode {
     NOT_FOUND_USER(1100, HttpStatus.BAD_REQUEST, "존재하지 않는 회원 정보입니다."),
     USER_ALREADY_EXIST(1101, HttpStatus.BAD_REQUEST, "이미 존재하는 회원 정보입니다."),
 
+    NOT_FOUND_GROUP(1001, HttpStatus.BAD_REQUEST, "해당 모임을 찾을 수 없습니다."),
     NONE_PARTICIPANT(1003, HttpStatus.NOT_FOUND, "존재하지 않는 참가자 정보입니다."),
     ALREADY_USE_NICKNAME(1004, HttpStatus.BAD_REQUEST, "모임에서 이미 사용중인 닉네임입니다."),
     ALREADY_INTO_GROUP(1006, HttpStatus.BAD_REQUEST, "이미 참여중인 모임입니다.");
