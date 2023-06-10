752d3efca54b5b9f4cc0d8b9c81ec50bc1efe569
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-06-11 03:09:37 +0900

    feat : Group 수정 Request 객체 생성

---
+++ src/main/java/com/sosim/server/group/dto/request/UpdateGroupRequest.java
@@ -0,0 +1,27 @@
+package com.sosim.server.group.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+public class UpdateGroupRequest {
+    @NotBlank
+    @Size(min = 1, max = 15, message = "모임 이름은 최소 1글자, 최대 15글자까지 허용됩니다.")
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("nickname")
+    private String nickname;
+
+    @NotNull
+    @JsonProperty("type")
+    private String groupType;
+
+    @NotNull
+    @JsonProperty("coverColor")
+    private String coverColorType;
+}
