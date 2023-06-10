72cc108f17163b9237f142cdc2655b872f01249d
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-06-10 18:03:08 +0900

    feat : Group Entity 조회 쿼리 추가

--- src/main/java/com/sosim/server/group/GroupRepository.java
+++ src/main/java/com/sosim/server/group/GroupRepository.java
@@ -1,6 +1,11 @@
 package com.sosim.server.group;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
+    @Query("select g from Group g where g.id = :groupId and status = 'ACTIVE'")
+    Optional<Group> findById(Long groupId);
 }
