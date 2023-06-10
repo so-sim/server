575472a67767c70296491cc4dbfc3fbb15904ee4
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-06-10 15:44:51 +0900

    feat : Group 생성 API 구현

--- src/main/java/com/sosim/server/common/response/ResponseCode.java
+++ src/main/java/com/sosim/server/common/response/ResponseCode.java
@@ -13,6 +13,10 @@ public enum ResponseCode {
     SUCCESS_REFRESH_TOKEN(successCode(), HttpStatus.OK, "토큰이 성공적으로 재발급 되었습니다."),
     SUCCESS_LOGOUT(successCode(), HttpStatus.OK, "로그아웃이 성공적으로 완료되었습니다."),
 
+    CREATE_GROUP(successCode(), HttpStatus.CREATED, "모임이 성공적으로 생성되었습니다."),
+
+    BINDING_ERROR(2000, HttpStatus.BAD_REQUEST, "입력값 중 검증에 실패한 값이 있습니다"),
+
     NOT_EXIST_TOKEN_COOKIE(1200, HttpStatus.BAD_REQUEST, "리프레시 토큰이 존재하지 않습니다."),
 
     NOT_FOUND_USER(1100, HttpStatus.BAD_REQUEST, "존재하지 않는 회원 정보입니다."),
@@ -24,6 +28,6 @@ public enum ResponseCode {
     private String message;
 
     private static int successCode() {
-        return 900;
+        return 1000;
     }
 }
--- src/main/java/com/sosim/server/group/GroupController.java
+++ src/main/java/com/sosim/server/group/GroupController.java
@@ -1,4 +1,40 @@
 package com.sosim.server.group;
 
+import com.sosim.server.common.advice.exception.CustomException;
+import com.sosim.server.common.response.Response;
+import com.sosim.server.common.response.ResponseCode;
+import com.sosim.server.group.dto.request.CreateGroupRequest;
+import com.sosim.server.group.dto.response.CreateGroupResponse;
+import com.sosim.server.security.AuthUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class GroupController {
+
+    private final GroupService groupService;
+
+    @PostMapping("/group")
+    public ResponseEntity<?> createGroup(@AuthenticationPrincipal AuthUser authUser,
+                                         @Validated @RequestBody CreateGroupRequest createGroupRequest,
+                                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new CustomException(ResponseCode.BINDING_ERROR, bindingResult.getFieldError().getField(),
+                    bindingResult.getFieldError().getDefaultMessage());
+        }
+
+        CreateGroupResponse createGroupResponse = groupService.createGroup(authUser.getId(), createGroupRequest);
+        ResponseCode createGroup = ResponseCode.CREATE_GROUP;
+
+        return new ResponseEntity<>(Response.create(createGroup, createGroupResponse), createGroup.getHttpStatus());
+    }
 }
