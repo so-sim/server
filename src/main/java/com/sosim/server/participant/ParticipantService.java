054e0a3c9f8c1f36cdb839a6744144edef27ab48
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-06-10 16:07:22 +0900

    feat : Participant 생성 로직 구현

---
+++ src/main/java/com/sosim/server/participant/ParticipantService.java
@@ -0,0 +1,31 @@
+package com.sosim.server.participant;
+
+import com.sosim.server.common.advice.exception.CustomException;
+import com.sosim.server.common.response.ResponseCode;
+import com.sosim.server.group.Group;
+import com.sosim.server.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ParticipantService {
+
+    private final ParticipantRepository participantRepository;
+
+    public void creteParticipant(User userEntity, Group groupEntity, String nickname) {
+        if (participantRepository.existsByUserAndGroup(userEntity, groupEntity)) {
+            throw new CustomException(ResponseCode.ALREADY_INTO_GROUP);
+        }
+
+        if (participantRepository.existsByGroupAndNickname(groupEntity, nickname)) {
+            throw new CustomException(ResponseCode.ALREADY_USE_NICKNAME);
+        }
+
+        saveParticipantEntity(Participant.create(userEntity, groupEntity, nickname));
+    }
+
+    public void saveParticipantEntity(Participant participant) {
+        participantRepository.save(participant);
+    }
+}
