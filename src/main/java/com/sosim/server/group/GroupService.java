38b83bc46415c763ea33201e0b92db5540c5c519
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-06-10 15:40:38 +0900

    feat : Group 생성 로직 구현

--- src/main/java/com/sosim/server/group/GroupService.java
+++ src/main/java/com/sosim/server/group/GroupService.java
@@ -1,4 +1,23 @@
 package com.sosim.server.group;
 
+import com.sosim.server.group.dto.request.CreateGroupRequest;
+import com.sosim.server.group.dto.response.CreateGroupResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class GroupService {
+
+    private final GroupRepository groupRepository;
+
+    public CreateGroupResponse createGroup(Long userId, CreateGroupRequest createGroupRequest) {
+        Group groupEntity = saveGroupEntity(Group.create(userId, createGroupRequest));
+
+        return CreateGroupResponse.create(groupEntity);
+    }
+
+    public Group saveGroupEntity(Group group) {
+        return groupRepository.save(group);
+    }
 }
