d616a1f2d225fdb3d5c9325314d430030c3f4047
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-06-14 17:26:56 +0900

    feat : 쿼리문 수정 및 방장인 그룹 목록 조회 추가

--- src/main/java/com/sosim/server/group/GroupRepository.java
+++ src/main/java/com/sosim/server/group/GroupRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
     @Query("select g from Group g where g.id = :groupId and status = 'ACTIVE'")
     Optional<Group> findById(@Param("groupId") Long groupId);
+
+    @Query("select g from Group g where g.adminId in (:adminId) and status = 'ACTIVE'")
+    List<Group> findListByAdminId(@Param("adminId") Long id);
 }
--- src/main/java/com/sosim/server/user/UserRepository.java
+++ src/main/java/com/sosim/server/user/UserRepository.java
@@ -2,9 +2,12 @@ package com.sosim.server.user;
 
 import com.sosim.server.oauth.Social;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findBySocialAndSocialId(Social social, Long socialId);
+    @Query("select u from User u where u.social = :social and u.socialId = :socialId and u.status = 'ACTIVE'")
+    Optional<User> findBySocialAndSocialId(@Param("social") Social social, @Param("socialId") Long socialId);
 }
