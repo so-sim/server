fde7a1a3bcfaec0c6103c664e3392e3f4b486086
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-06-14 18:11:09 +0900

    feat : Event 기본 도메인 설계 Layer 추가

---
+++ src/main/java/com/sosim/server/event/Event.java
@@ -0,0 +1,55 @@
+package com.sosim.server.event;
+
+import com.sosim.server.group.Group;
+import com.sosim.server.user.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "EVENT")
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "EVENT_ID")
+    private Long id;
+
+    @Column(name = "DATE")
+    private LocalDate date;
+
+    @Column(name = "AMOUNT")
+    private int amount;
+
+    @Column(name = "GROUND")
+    private String ground;
+
+    @Column(name = "MEMO")
+    private String memo;
+
+    @Column(name = "SITUATION")
+    private String situation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "GROUP_ID")
+    private Group group;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @Builder
+    private Event(LocalDate date, int amount, String ground, String memo, String situation,
+                  Group group, User user) {
+        this.amount = amount;
+        this.ground = ground;
+        this.memo = memo;
+        this.situation = situation;
+        this.group = group;
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/sosim/server/event/EventService.java
@@ -0,0 +1,9 @@
+package com.sosim.server.event;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EventService {
+}
