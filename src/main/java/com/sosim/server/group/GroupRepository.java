def1729e56cb4f1be0590610216532e596498475
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-06-15 15:27:55 +0900

    fix : N+1 문제 수정 Fetch Join Query 추가

--- src/main/java/com/sosim/server/group/GroupRepository.java
+++ src/main/java/com/sosim/server/group/GroupRepository.java
@@ -8,9 +8,13 @@ import java.util.List;
 import java.util.Optional;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
-    @Query("select g from Group g where g.id = :groupId and status = 'ACTIVE'")
+    @Query("select g from Group g where g.id = :groupId and g.status = 'ACTIVE'")
     Optional<Group> findById(@Param("groupId") Long groupId);
 
-    @Query("select g from Group g where g.adminId in (:adminId) and status = 'ACTIVE'")
+    @Query("select g from Group g where g.adminId in (:adminId) and g.status = 'ACTIVE'")
     List<Group> findListByAdminId(@Param("adminId") Long id);
+
+    @Query("select g from Group g " +
+            "join fetch g.participantList where g.adminId in (:adminId) and g.status = 'ACTIVE'")
+    List<Group> findFetchJoinGroupByAdminId(@Param("adminId") Long groupId);
 }
