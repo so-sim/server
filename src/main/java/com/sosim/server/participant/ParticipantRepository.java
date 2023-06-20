85c4354d3b6418fb6646472d80852aed9f0b3590
김현교 <zkdlajt96@gmail.com>
2023-06-20 19:31:22 +0900

    chore : 안 쓰는 쿼리 삭제

--- src/main/java/com/sosim/server/participant/ParticipantRepository.java
+++ src/main/java/com/sosim/server/participant/ParticipantRepository.java
@@ -1,8 +1,6 @@
 package com.sosim.server.participant;
 
 import com.sosim.server.common.auditing.Status;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,17 +16,9 @@ public interface ParticipantRepository extends JpaRepository<Participant, java.l
             "and p.group.id = :groupId and p.status = 'ACTIVE'")
     Optional<Participant> findByUserIdAndGroupId(@Param("userId") Long userId, @Param("groupId") Long groupId);
 
-   @Query("select p from Participant p where p.nickname = :nickname " +
+    @Query("select p from Participant p where p.nickname = :nickname " +
            "and p.group.id = :groupId and p.status = 'ACTIVE'")
     Optional<Participant> findByNicknameAndGroupId(@Param("nickname") String nickname, @Param("groupId") Long groupId);
-
-   @Query("select p from Participant p where p.user.id = :userId " +
-           "and p.status = 'ACTIVE' order by p.id desc")
-   Slice<Participant> findByUserIdOrderByIdDesc(@Param("userId") Long userId, Pageable pageable);
-
-   @Query("select p from Participant p where p.id < :participantId " +
-           "and p.user.id = :userId and p.status = 'ACTIVE' order by p.id desc")
-   Slice<Participant> findByIdLessThanAndUserIdOrderByIdDesc(@Param("participantId")Long participantId, @Param("userId")Long userId, Pageable pageable);
 
     @Query("SELECT p FROM Participant p " +
             "WHERE p.group.id = :groupId " +
@@ -37,8 +27,4 @@ public interface ParticipantRepository extends JpaRepository<Participant, java.l
             "ORDER BY p.nickname ASC")
     List<Participant> findGroupNormalParticipants(@Param("groupId") long groupId, @Param("adminNickname") String adminNickname);
 
-    //TODO: 쿼리 테스트
-    @Query("SELECT p FROM Participant p " +
-            "WHERE p.user.id = :userId AND p.status = 'ACTIVE'")
-    Slice<Participant> findByUserId(@Param("userId") long userId, Pageable pageable);
 }
