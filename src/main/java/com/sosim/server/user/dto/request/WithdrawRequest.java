ff19deaf717d3986ca58f293f867d6f3d54c8842
김현교 <zkdlajt96@gmail.com>
2023-06-21 11:02:13 +0900

    test: 회원 탈퇴 테스트

--- src/main/java/com/sosim/server/user/dto/request/WithdrawRequest.java
+++ src/main/java/com/sosim/server/user/dto/request/WithdrawRequest.java
@@ -1,9 +1,8 @@
 package com.sosim.server.user.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
+import lombok.Data;
 
-@Getter
+@Data
 public class WithdrawRequest {
     private String withdrawReason;
 }
--- src/test/java/com/sosim/server/user/UserControllerTest.java
+++ src/test/java/com/sosim/server/user/UserControllerTest.java
@@ -4,20 +4,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sosim.server.common.advice.exception.CustomException;
 import com.sosim.server.security.WithMockCustomUser;
 import com.sosim.server.security.WithMockCustomUserSecurityContextFactory;
+import com.sosim.server.user.dto.request.WithdrawRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static com.sosim.server.common.response.ResponseCode.*;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -76,4 +80,76 @@ class UserControllerTest {
                 .andExpect(jsonPath("$.status.code").value(CANNOT_WITHDRAWAL_BY_GROUP_ADMIN.getCode()))
                 .andExpect(jsonPath("$.status.message").value(CANNOT_WITHDRAWAL_BY_GROUP_ADMIN.getMessage()));
     }
+
+    @WithMockCustomUser
+    @DisplayName("회원 탈퇴 / 성공 -> SUCCESS_WITHDRAW")
+    @Test
+    void user_withdraw() throws Exception {
+        //given
+        String withdrawReason = "탈퇴 사유";
+        WithdrawRequest request = makeWithdrawRequest(withdrawReason);
+
+        doNothing().when(userService).withdrawUser(userId, request);
+
+        //when
+        ResultActions actions = withdrawAction(withdrawReason);
+
+        //then
+        actions.andExpect(status().isOk())
+                .andExpect(jsonPath("$.status.code").value(SUCCESS_WITHDRAW.getCode()))
+                .andExpect(jsonPath("$.status.message").value(SUCCESS_WITHDRAW.getMessage()));
+    }
+
+    @WithMockCustomUser
+    @DisplayName("회원 탈퇴 / Admin 참가자 기록이 있는 경우 -> CANNOT_WITHDRAWAL_BY_GROUP_ADMIN")
+    @Test
+    void user_withdraw_has_admin_data() throws Exception {
+        //given
+        String withdrawReason = "탈퇴 사유";
+        WithdrawRequest request = makeWithdrawRequest(withdrawReason);
+
+        CustomException e = new CustomException(CANNOT_WITHDRAWAL_BY_GROUP_ADMIN);
+        doThrow(e).when(userService).withdrawUser(userId, request);
+
+        //when
+        ResultActions actions = withdrawAction(withdrawReason);
+
+        //then
+        actions.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status.code").value(CANNOT_WITHDRAWAL_BY_GROUP_ADMIN.getCode()))
+                .andExpect(jsonPath("$.status.message").value(CANNOT_WITHDRAWAL_BY_GROUP_ADMIN.getMessage()));
+    }
+
+    @WithMockCustomUser
+    @DisplayName("회원 탈퇴 / User가 없는 경우 -> NOT_FOUND_USER")
+    @Test
+    void user_withdraw_no_user() throws Exception {
+        //given
+        String withdrawReason = "탈퇴 사유";
+        WithdrawRequest request = makeWithdrawRequest(withdrawReason);
+
+        CustomException e = new CustomException(NOT_FOUND_USER);
+        doThrow(e).when(userService).withdrawUser(userId, request);
+
+        //when
+        ResultActions actions = withdrawAction(withdrawReason);
+
+        //then
+        actions.andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.status.code").value(NOT_FOUND_USER.getCode()))
+                .andExpect(jsonPath("$.status.message").value(NOT_FOUND_USER.getMessage()));
+    }
+
+    private ResultActions withdrawAction(String withdrawReason) throws Exception {
+        ResultActions actions = mvc.perform(delete(URI_PREFIX)
+                .param("withdrawReason", withdrawReason));
+        return actions;
+    }
+
+    private static WithdrawRequest makeWithdrawRequest(String withdrawReason) {
+        WithdrawRequest request = new WithdrawRequest();
+        ReflectionTestUtils.setField(request, "withdrawReason", withdrawReason);
+        return request;
+    }
+
 }
