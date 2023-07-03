cb03933735e801ee1d053c949c1fe617338e8f55
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-07-03 21:37:50 +0900

    fix : groupId Path 의 GET 요청만 권한 수정

--- src/main/java/com/sosim/server/config/SecurityConfig.java
+++ src/main/java/com/sosim/server/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.sosim.server.security.filter.AuthenticationFilter;
 import com.sosim.server.security.filter.JwtFailureFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -42,7 +43,7 @@ public class SecurityConfig {
         // 요청에 대한 권한 체크 파트
         http
                 .authorizeRequests()
-                .antMatchers("/api/group/{groupId}").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/group/{groupId}").permitAll()
                 .antMatchers("/api/**").authenticated()
                 .antMatchers("/**").permitAll();
 
