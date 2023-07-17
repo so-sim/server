1beb3759254efe8186019a6f63403a2d1b7dedea
jaemin-park <dhkdhkdhkdn@gmail.com>
2023-07-17 19:40:01 +0900

    modify : 메서드명 수정

--- src/main/java/com/sosim/server/common/util/SituationConverter.java
+++ src/main/java/com/sosim/server/common/util/SituationConverter.java
@@ -6,6 +6,6 @@ import org.springframework.core.convert.converter.Converter;
 public class SituationConverter implements Converter<String, Situation> {
     @Override
     public Situation convert(String situation) {
-        return Situation.getGround(situation);
+        return Situation.getSituation(situation);
     }
 }
