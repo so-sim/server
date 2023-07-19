e65e5d79c19cc79b9cf058d2f8825e09d28e2212
김현교 <zkdlajt96@gmail.com>
2023-07-19 21:15:46 +0900

    fix: 잘못된 수정 커밋 취소

--- src/main/java/com/sosim/server/event/EventRepositoryImpl.java
+++ src/main/java/com/sosim/server/event/EventRepositoryImpl.java
@@ -58,7 +58,7 @@ public class EventRepositoryImpl implements EventRepositoryDsl {
     }
 
     private BooleanExpression equalsSituation(Situation situation) {
-        return situation == null ? null : event.situation.eq(situation.name());
+        return situation == null ? null : event.situation.eq(situation);
     }
 
     private Page<Event> doPageable(JPAQuery<Event> filterEvents, Pageable pageable, long totalSize) {
