6e78bf8fcc3bdcaf6b3b7d2c83aa0a90ca8c99c6
김현교 <zkdlajt96@gmail.com>
2023-07-20 18:14:33 +0900

    fix: Query 오류로 인한 테스트 실패 수정

--- src/main/java/com/sosim/server/notification/NotificationRepository.java
+++ src/main/java/com/sosim/server/notification/NotificationRepository.java
@@ -21,17 +21,17 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
             "WHERE n.userId = :userId")
     void updateViewByUserId(@Param("userId") long userId);
 
-    @Query("SELECT * FROM Notification n " +
+    @Query(value = "SELECT * FROM Notification n " +
             "WHERE n.userId = :userId " +
             "AND n.reserved = false " +
-            "AND n.sentDateTime >= :time")
+            "AND n.sendDateTime >= :time", nativeQuery = true)
     Slice<Notification> findMyNotifications(@Param("userId") long userId, @Param("time") LocalDateTime time, Pageable pageable);
 
-    @Query("SELECT * FROM Notification n " +
-            "WHERE n.sentDate <= CURRENT_TIMESTAMP AND n.reserved = true ")
+    @Query(value = "SELECT * FROM Notification n " +
+            "WHERE n.sendDateTime <= CURRENT_TIMESTAMP AND n.reserved = true ", nativeQuery = true)
     List<Notification> findReservedNotifications();
 
     @Query("DELETE FROM Notification n " +
-            "WHERE n.groupId = :groupId AND n.reserved = true")
+            "WHERE n.groupInfo.groupId = :groupId AND n.reserved = true")
     void deleteReservedNotifications(@Param("groupId") long groupId);
 }
